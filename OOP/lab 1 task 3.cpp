b70b89b6346e349cebed7032c5bb1b6953070f62
Danish Zulfiqar <102870087+danishzulfiqar@users.noreply.github.com>
2022-09-25 11:55:30 +0500

    Add files via upload

---
+++ OOP/lab 1 task 3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+#include <bits/stdc++.h>
+using namespace std;
+
+void passbyvalue(int a,int b)
+{
+    int temp;
+    temp = a;
+    a=b;
+    b=temp;
+
+    cout<<"Pass by value prints in the function"<<endl;
+    cout<<a<<" "<<b<<endl;
+
+}void passbyref(int &a,int &b)
+{
+    int temp;
+    temp = a;
+    a=b;
+    b=temp;
+}
+
+int main()
+{
+    int num1,num2;
+    cin>>num1>>num2;
+
+    passbyvalue(num1,num2); //original values dont change
+
+    passbyref(num1,num2);
+
+    cout<<"\nPass by Reference Changes Original Values"<<endl;
+    cout<<num1<<" "<<num2<<endl;
+
+    return 0;
+
+}// Author: Danish Zulfiqar
